cdc5785be524bc726aa8057263446d18c3ad610c
daniil <daniil.kvashnin@gmail.com>
2022-07-12 20:41:43 +0300

    initial commit

---
+++ src/test/kotlin/com/portfolio/wizard/tickerscanner/TickerScannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.portfolio.wizard.tickerscanner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TickerScannerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
